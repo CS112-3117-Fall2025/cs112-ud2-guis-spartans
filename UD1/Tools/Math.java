5e085e5f5e00d40ec7ec6de2f6f8922f1b070a90
Andrew Ibarra <microplex101@icloud.com>
2025-10-10 23:24:26 +0000

    Setup from UD1

---
+++ UD1/Tools/Math.java
@@ -0,0 +1,20 @@
+package Tools;
+
+/**
+ * A class used to store commonly used math operations in this program.
+ * 
+ * @author Andrew Ibarra
+ * @version 1.0
+ */
+public class Math {
+
+    public static double roundDouble(double number, int decimalPlace) {
+
+        double roundNearest = 1 / decimalPlace,
+                remainder = number % roundNearest,
+                truncated = number - remainder;
+
+        return remainder >= 0.5 * roundNearest ? truncated + roundNearest : truncated;
+    }
+
+}
